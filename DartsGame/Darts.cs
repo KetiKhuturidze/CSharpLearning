71cb4a508bff418904f30e17097ec13d42e68192
ketikh <qxuturidze@mail.ru>
2024-05-14 02:30:56 +0400

    adding c# projects

---
+++ DartsGame/Darts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DartsGame
+{
+    public static class Darts
+    {
+        public static int GetScore(double x, double y)
+        {
+            double distance = Math.Sqrt((x * x) + (y * y));
+
+            if (distance > 10)
+            {
+                return 0;
+            }
+            else if (distance <= 1)
+            {
+                return 10;
+            }
+            else if (distance <= 5)
+            {
+                return 5;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
---
+++ LeapYear/Year.cs
@@ -0,0 +1,22 @@
+﻿namespace LeapYearTask
+{
+    public static class Year
+    {
+        public static bool IsLeapYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0 && year % 400 != 0)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
